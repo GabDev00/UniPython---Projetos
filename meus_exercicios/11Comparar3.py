129b4c7bde83b948d6b14ad527db976285861db6
Gabriel Cardoso <cardoso.gabriel5332@gmail.com>
2026-03-18 00:33:29 -0300

    exercicio 11 feito

--- meus_exercicios/11Comparar3.py
+++ meus_exercicios/11Comparar3.py
@@ -7,12 +7,25 @@ num1 = int(input("Informe o primeiro número "))
 num2 = int(input("Informe o segundo número "))
 num3 = int(input("Informe o terceiro número "))
 
-maior = num1
+#maior = num1
 
-if num2 > maior:
-    maior = num2
+# if num2 > maior:
+#     maior = num2
     
-if num3 > maior:
-    maior = num3
+# if num3 > maior:
+#     maior = num3
     
-print("O maior número é:", maior)         
+# print("O maior número é:", maior)         
+
+def maior_de_tres(num1, num2, num3):
+    maior = num1
+
+    if num2 > maior:
+        maior = num2
+
+    if num3 > maior:
+        maior = num3
+
+    return f"O maior número é {maior}"   
+
+print(maior_de_tres(num1, num2, num3))     
